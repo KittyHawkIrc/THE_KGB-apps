b93273a5a60f66b838c902bd3b23ba3a6b8a8d0d
nonillion <jadelyjade1@gmail.com>
2016-01-11 12:55:35 -0500

    forgot to replace print back to self.msg

--- pimp.py
+++ pimp.py
@@ -26,7 +26,7 @@ def callback(self, type, isop, command="", msg="", user="", channel="", mode="")
             pimpdb[chan] = {}
 
         if u == target and com != 'get':
-            print(chan, "%s: Y'all can't pimp yourself, ma nigga" % (u))
+            self.msg(chan, "%s: Y'all can't pimp yourself, ma nigga" % (u))
             return
 
         if com == '+1':
@@ -37,7 +37,7 @@ def callback(self, type, isop, command="", msg="", user="", channel="", mode="")
             elif pimpdb[chan][u] > 0:
                 pimpdb[chan][u] = int(pimpdb[chan][u]) - 1
             else:
-                print(channel, "%s: You ain't got no pimp points" % (u))
+                self.msg(channel, "%s: You ain't got no pimp points" % (u))
                 return
 
             try: #modifies users points
@@ -53,7 +53,7 @@ def callback(self, type, isop, command="", msg="", user="", channel="", mode="")
             elif pimpdb[chan][u] > 0:
                 pimpdb[chan][u] = pimpdb[chan][u] - 1
             else:
-                print(channel, "%s: You ain't got no pimp points" % (u))
+                self.msg(channel, "%s: You ain't got no pimp points" % (u))
                 return
 
             try: #also modifies users pounts
@@ -65,14 +65,14 @@ def callback(self, type, isop, command="", msg="", user="", channel="", mode="")
             try:
                 pimp = pimpdb[chan][target]
                 if pimp > 0:
-                    print(channel, "that nigga got %s pimp points up in this bitch" % (pimp))
+                    self.msg(channel, "that nigga got %s pimp points up in this bitch" % (pimp))
                 else:
-                    print(channel, "that nigga got %s bitch points" % (pimp))
+                    self.msg(channel, "that nigga got %s bitch points" % (pimp))
             except:
-                print(channel, "that nigga ain't got no pimp points")
+                self.msg(channel, "that nigga ain't got no pimp points")
 
         else:
-            print(channel, "%s: Nigga, you really think you that og, just giving out more points like that?" % (u))
+            self.msg(channel, "%s: Nigga, you really think you that og, just giving out more points like that?" % (u))
 
     elif isop:
         var = msg.lower().split()
@@ -93,32 +93,32 @@ def callback(self, type, isop, command="", msg="", user="", channel="", mode="")
         if com == 'set':
             try:
                 pimpdb[chan][target] = int(val)
-                print(u, "%s's score in %s is now set to %s" % (target, chan, val))
+                self.msg(u, "%s's score in %s is now set to %s" % (target, chan, val))
             except:
-                print(u, "%s doesn't exist in %s" % (target, chan))
+                self.msg(u, "%s doesn't exist in %s" % (target, chan))
         elif com == 'add':
             try:
                 pimpdb[chan][target] += val
-                print(u, "%s's score in %s is now set to %s" % (target, chan, val))
+                self.msg(u, "%s's score in %s is now set to %s" % (target, chan, val))
             except:
-                print("Doesn't %s exist in %s" % (target, chan))
+                self.msg("Doesn't %s exist in %s" % (target, chan))
         elif com == 'remove':
             if target in pimpdb[chan]:
                 pimpdb[chan].pop(target)
-                print(u, "%s is now removed from that channel's list" % (target))
+                self.msg(u, "%s is now removed from that channel's list" % (target))
             else:
-                print(u, "%s is not in that channel's list")
+                self.msg(u, "%s is not in that channel's list")
         elif com == 'new':
             if chan not in pimpdb:
                 pimpdb[chan] = {}
             if target not in pimpdb[chan]:
                 pimpdb[chan][target] = int(val)
-                print(u, "%s is now added to the channel with %s points" % (target, val))
+                self.msg(u, "%s is now added to the channel with %s points" % (target, val))
             else:
-                print(u, "%s is already in the channel with %s points, please remove them before adding them again" % (target, val))
+                self.msg(u, "%s is already in the channel with %s points, please remove them before adding them again" % (target, val))
 
         elif com == 'dump':
-            print(u, str(pimpdb))
+            self.msg(u, str(pimpdb))
         elif com == 'load':
             pimpdb = eval(msg.lower().split('load')[1])
         elif com == 'inflate': #target == value to multiply by
@@ -131,4 +131,4 @@ def callback(self, type, isop, command="", msg="", user="", channel="", mode="")
             for k,v in pimpdb[chan].items():
                 pimpdb[chan][k] += int(target)
         else:
-            print(u, "unavailable")
+            self.msg(u, "unavailable")
