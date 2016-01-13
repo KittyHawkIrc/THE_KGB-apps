934841eb9a169b39cfc50c37779ad72992ba5cdb
nonillion <jadelyjade1@gmail.com>
2016-01-13 04:01:38 -0500

    added use of ternary operator

--- hello.py
+++ hello.py
@@ -4,8 +4,4 @@ def declare():
 def callback(self, type, isop, command="", msg="", user="", channel="", mode=""):
 
     if channel.startswith('#'):
-
-        if isop:
-            self.msg(channel, "And a hello to you too, operator %s!" % (user))
-        else:
-            self.msg(channel, "And a hello to you too, %s!" % (user))
+        self.msg(channel, "And a hello to you too, " + ("operator" if isop else "user") + " %s!" % (user))
