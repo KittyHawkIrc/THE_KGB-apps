39e80e9398dcda27db323d9b238bc8c323881cd5
nonillion <jadelyjade1@gmail.com>
2016-01-12 22:58:53 -0500

    Update brainfuck.py

--- brainfuck.py
+++ brainfuck.py
@@ -75,4 +75,4 @@ if __name__ == "__main__":
 	if callback(api, type, isop, command=hook, msg="^brainfuck ++++++++++++++++++++++++++++++++++++++++++++++++++++++++..............", channel=channel, user=user) != "88888888888888":
 		exit(1)
 	if callback(api, type, isop, command=hook, msg="^brainfuck +++++ +++++ initialize counter (cell #0) to 10 [ use loop to set 70/100/30/10 > +++++ ++ add 7 to cell #1 > +++++ +++++ add 10 to cell #2 > +++ add 3 to cell #3 > + add 1 to cell #4 <<<< - decrement counter (cell #0) ] > ++ . print 'H' > + . print 'e' +++++ ++ . print 'l' . print 'l' +++ . print 'o' > ++ . print ' ' << +++++ +++++ +++++ . print 'W' > . print 'o' +++ . print 'r' ----- - . print 'l' ----- --- . print 'd' > + . print '!' > . print '\n'", channel="", user=user) != "Hello World!":
-		exit(1)
+		exit(2)
