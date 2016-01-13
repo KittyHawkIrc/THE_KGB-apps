9fd5f0fa3ccec343371411dc0e26c2adc7b819ac
nonillion <jadelyjade1@gmail.com>
2016-01-13 00:06:22 -0500

    fixed some error and typos
    Best one was using the variable index where I should have used the variable i. Simple mistakes make me look dumb ^_^

--- brainfuck.py
+++ brainfuck.py
@@ -6,31 +6,35 @@ def callback(self, type, isop, command="", msg="", user="", channel="", mode="")
 		DEFAULT_COUNTER = 1000000
 		counter = DEFAULT_COUNTER
 		index = 0
-		array = [0] * 30000
+		array = [0] * 20
 		lastRepeat = []
 		output = ""
 		disabled = -1
+		i = 0
 		c = 0
 		
-		while c > len(msg):
-			c += 1
-			
+		while i < len(msg):
+			c = msg[i]
+			i += 1
+			counter -= 1
+	
 			if counter <= 0:
-				self.msg(channel, "Command exceeded %s ticks." % (DEFAULT_COUNTER)) 
+				self.msg(channel, "Command exceeded %s ticks." % (DEFAULT_COUNTER))
+				return("Command exceeded %s ticks." % (DEFAULT_COUNTER)) 
 			
 			if c == '[':
 				if array[index] == 0:
-					c+=1
 					disabled = len(lastRepeat)
-				lastRepeat.append(c)
+				lastRepeat.append(i)
 			elif c == ']':
 				if array[index] == 0:
-					lastRepeat.pop(c)
+					lastRepeat.pop()
 					if len(lastRepeat) <= disabled:
 						disabled = -1
 				else:
-					index = lastRepeat.pop(c)
-			elif disabled >= 0:
+					i = lastRepeat.pop()-1
+			elif disabled > 0:
+				#print("disabled")
 				if len(lastRepeat) > disabled:
 					continue
 				else:
@@ -45,16 +49,22 @@ def callback(self, type, isop, command="", msg="", user="", channel="", mode="")
 			elif c == '<':
 				index -= 1
 			elif c == '.':
+				#print(".")
 				output += chr(array[index])
 			else:
 				continue
+	
+			#print(c+":"+str(index)+":"+str(array))
 			
+			#try:
 			if array[index] > 255:
 				array[index] = 0
 			elif array[index] < 0:
 				array[index] = 255
-		
+	
+		self.msg(channel, output[:133])
 		return output
+
 
 class api:
 	def msg(self, channel, text):
@@ -73,9 +83,15 @@ if __name__ == "__main__":
 	
 	t1 = callback(api, type, isop, command=hook, msg="^brainfuck ++++++++++++++++++++++++++++++++++++++++++++++++++++++++..............", channel=channel, user=user)
 	t2 = callback(api, type, isop, command=hook, msg="^brainfuck +++++ +++++ initialize counter (cell #0) to 10 [ use loop to set 70/100/30/10 > +++++ ++ add 7 to cell #1 > +++++ +++++ add 10 to cell #2 > +++ add 3 to cell #3 > + add 1 to cell #4 <<<< - decrement counter (cell #0) ] > ++ . print 'H' > + . print 'e' +++++ ++ . print 'l' . print 'l' +++ . print 'o' > ++ . print ' ' << +++++ +++++ +++++ . print 'W' > . print 'o' +++ . print 'r' ----- - . print 'l' ----- --- . print 'd' > + . print '!' > . print '\n'", channel="", user=user)
+	t3 = callback(api, type, isop, command=hook, msg="^brainfuck ++[++++.]", channel=channel, user=user)
+	t4 = callback(api, type, isop, command=hook, msg="-[-.]", channel=channel, user=user)
 	
 	callback(api, type, isop, command=hook, msg=msg, channel=channel, user=user)
 	if t1 != "88888888888888":
 		exit(1)
-	if t2 != "Hello World!":
+	if t2 != "Hello World!\n":
 		exit(2)
+	if t3 != 'Command exceeded 1000000 ticks.':
+		exit(3)
+	if t4 != 'þýüûúùø÷öõôóòñðïîíìëêéèçæåäãâáàßÞÝÜÛÚÙØ×ÖÕÔÓÒÑÐÏÎÍÌËÊÉÈÇÆÅÄÃÂÁÀ¿¾½¼»º¹¸·¶µ´³²±°¯®\xad¬«ª©¨§¦¥¤£¢¡\xa0\x9f\x9e\x9d\x9c\x9b':
+		exit(4)
