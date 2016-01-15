552259c1b2185739dd4abd74bd13b198b7091f3f
nonillion <jadelyjade1@gmail.com>
2016-01-14 21:46:42 -0500

    initial commit to use self.locker

--- pimp.py
+++ pimp.py
@@ -1,4 +1,3 @@
-import __builtin__
 import urllib2
 from time import ctime
 
@@ -7,33 +6,32 @@ from time import ctime
 #t = target who's points are being modified
 #p = points being taken/removed
 def addPoints(api, c, u, t, p):
-	__builtin__.pimpdb
 	ts = ctime()
 	up = 0
 	tp = 0
 
 	assert p == 1 or p == -1
 
-	if u not in __builtin__.pimpdb[c]:
-		__builtin__.pimpdb[c][u] = 4
-		pimpToChan(api, "%s <%s> %s added to db with %s points" % (ts,c,u,__builtin__.pimpdb[c][u]))
-	elif __builtin__.pimpdb[c][u] > 0:
-		__builtin__.pimpdb[c][u] = int(__builtin__.pimpdb[c][u]) - 1
+	if u not in self.locker.pimpdb[c]:
+		self.locker.pimpdb[c][u] = 4
+		pimpToChan(api, "%s <%s> %s added to db with %s points" % (ts,c,u,self.locker.pimpdb[c][u]))
+	elif self.locker.pimpdb[c][u] > 0:
+		self.locker.pimpdb[c][u] = int(self.locker.pimpdb[c][u]) - 1
 	else:
 		api.msg(channel, "%s: You ain't got no pimp points" % (u))
-		pimpToChan(api, "%s <%s> %s[%s]'s attempted to change %s[%s]'s points by %s" % (ts,c,u,__builtin__.pimpdb[c][u],t,__builtin__.pimpdb[c][t],p))
+		pimpToChan(api, "%s <%s> %s[%s]'s attempted to change %s[%s]'s points by %s" % (ts,c,u,self.locker.pimpdb[c][u],t,self.locker.pimpdb[c][t],p))
 		return
 
 	try: #modifies users points
-		__builtin__.pimpdb[c][t] += p
+		self.locker.pimpdb[c][t] += p
 	except:
-		__builtin__.pimpdb[c][t] = 5 + p
-		pimpToChan(api, "%s <%s> %s added to db with %s points" % (ts,c,t,__builtin__.pimpdb[c][t]-p))
+		self.locker.pimpdb[c][t] = 5 + p
+		pimpToChan(api, "%s <%s> %s added to db with %s points" % (ts,c,t,self.locker.pimpdb[c][t]-p))
 
-	pimpToChan(api, "%s <%s> %s[%s]'s changes %s[%s]'s points by %s" % (ts,c,u,__builtin__.pimpdb[c][u]-p,t,__builtin__.pimpdb[c][t]-p,p))
+	pimpToChan(api, "%s <%s> %s[%s]'s changes %s[%s]'s points by %s" % (ts,c,u,self.locker.pimpdb[c][u]-p,t,self.locker.pimpdb[c][t]-p,p))
 
 def pimpToChan(api, s):
-	for k,v in __builtin__.pimpToChandb.items():
+	for k,v in self.locker.pimpToChandb.items():
 		if v:
 			api.msg(k, s)
 
@@ -59,8 +57,8 @@ def callback(self, type, isop, command="", msg="", user="", channel="", mode="")
 		except:
 			com = 'get'
 
-		if chan not in __builtin__.pimpdb:
-			__builtin__.pimpdb[chan] = {}
+		if chan not in self.locker.pimpdb:
+			self.locker.pimpdb[chan] = {}
 
 		if u == target and com != 'get':
 			self.msg(chan, "%s: Y'all can't pimp yourself, ma nigga" % (u))
@@ -72,7 +70,7 @@ def callback(self, type, isop, command="", msg="", user="", channel="", mode="")
 			addPoints(self, chan, u, target, 1)
 		elif com == 'get':
 			try:
-				pimp = __builtin__.pimpdb[chan][target]
+				pimp = self.locker.pimpdb[chan][target]
 				if pimp > 0:
 					self.msg(channel, "that nigga got %s pimp points up in this bitch" % (pimp))
 				else:
@@ -81,13 +79,13 @@ def callback(self, type, isop, command="", msg="", user="", channel="", mode="")
 				pimp = 5
 				self.msg(channel, "that nigga got 5 pimp points up in this bitch")
 
-				if u in __builtin__.pimpdb[c]:
-					u_p = __builtin__.pimpdb[c][u]
+				if u in self.locker.pimpdb[c]:
+					u_p = self.locker.pimpdb[c][u]
 				else:
 					u_p = 5
 
-				if target in __builtin__.pimpdb[c]:
-					target_p = __builtin__.pimpdb[c][target]
+				if target in self.locker.pimpdb[c]:
+					target_p = self.locker.pimpdb[c][target]
 				else:
 					target_p = 5
 
@@ -114,8 +112,8 @@ def callback(self, type, isop, command="", msg="", user="", channel="", mode="")
 
 		if com == 'set':
 			try:
-				pval = __builtin__.pimpdb[chan][target]
-				__builtin__.pimpdb[chan][target] = int(val)
+				pval = self.locker.pimpdb[chan][target]
+				self.locker.pimpdb[chan][target] = int(val)
 				self.msg(u, "%s's score in %s is now set to %s" % (target, chan, val))
 				pimpToChan(self, "%s <%s> %s[%s]'s points set to %s by op %s" % (ts, chan, target, pval, val, u))
 			except:
@@ -123,16 +121,16 @@ def callback(self, type, isop, command="", msg="", user="", channel="", mode="")
 
 		elif com == 'add':
 			try:
-				__builtin__.pimpdb[chan][target] += val
+				self.locker.pimpdb[chan][target] += val
 				self.msg(u, "%s's score in %s is now set to %s" % (target, chan, val))
-				pimpToChan(self, "%s <%s> %s[%s]'s points increased by %s, by op %s" % (ts, chan, target, __builtin__.pimpdb[chan][target]-val, val, u))
+				pimpToChan(self, "%s <%s> %s[%s]'s points increased by %s, by op %s" % (ts, chan, target, self.locker.pimpdb[chan][target]-val, val, u))
 			except:
 				self.msg("Doesn't %s exist in %s" % (target, chan))
 
 		elif com == 'remove':
-			if target in __builtin__.pimpdb[chan]:
-				val = __builtin__.pimpdb[chan][target]
-				__builtin__.pimpdb[chan].pop(target)
+			if target in self.locker.pimpdb[chan]:
+				val = self.locker.pimpdb[chan][target]
+				self.locker.pimpdb[chan].pop(target)
 				self.msg(u, "%s is now removed from that channel's list" % (target))
 				pimpToChan(self, "%s <%s> %s removed from db with %s points by op %s" % (ts, chan, target, val, u))
 
@@ -140,11 +138,11 @@ def callback(self, type, isop, command="", msg="", user="", channel="", mode="")
 				self.msg(u, "%s is not in that channel's list")
 
 		elif com == 'new':
-			if chan not in __builtin__.pimpdb:
-				__builtin__.pimpdb[chan] = {}
+			if chan not in self.locker.pimpdb:
+				self.locker.pimpdb[chan] = {}
 
-			if target not in __builtin__.pimpdb[chan]:
-				__builtin__.pimpdb[chan][target] = int(val)
+			if target not in self.locker.pimpdb[chan]:
+				self.locker.pimpdb[chan][target] = int(val)
 				self.msg(u, "%s is now added to the channel with %s points" % (target, val))
 				pimpToChan(self, "%s <%s> %s added to db with %s points by op %s" % (ts, chan, target, val, u))
 
@@ -152,36 +150,36 @@ def callback(self, type, isop, command="", msg="", user="", channel="", mode="")
 				self.msg(u, "%s is already in the channel with %s points, please remove them before adding them again" % (target, val))
 
 		elif com == 'dump':
-			self.msg(u, str(__builtin__.pimpdb))
+			self.msg(u, str(self.locker.pimpdb))
 			pimpToChan(self, "%s <>  All channel values dumped by op %s" % (ts, u))
 
 		elif com == 'load':
 			req = urllib2.Request(msg.lower().split('load')[1])
 			fd = urllib2.urlopen(req)
-			__builtin__.pimpdb = eval(fd.read())
+			self.locker.pimpdb = eval(fd.read())
 			fd.close()
 			pimpToChan(self, "%s <> All channel values dumped by op %s" % (ts, u))
 
 		elif com == 'inflate': #target == value to multiply by
-			for k, v in __builtin__.pimpdb[chan].items():
+			for k, v in self.locker.pimpdb[chan].items():
 				if v > 0:
-					__builtin__.pimpdb[chan][k] *= int(target)
+					self.locker.pimpdb[chan][k] *= int(target)
 				else:
-					__builtin__.pimpdb[chan][k] /= int(target)
+					self.locker.pimpdb[chan][k] /= int(target)
 			pimpToChan(self, "%s <%s> Inflated by %s, by op %s" % (ts, chan, target, u))
 
 		elif com == 'addtoall': #target == value to add to each user
-			for k,v in __builtin__.pimpdb[chan].items():
-				__builtin__.pimpdb[chan][k] += int(target)
+			for k,v in self.locker.pimpdb[chan].items():
+				self.locker.pimpdb[chan][k] += int(target)
 			pimpToChan(self, "%s <%s> Increased by %s, by op %s" % (ts, chan, target, u))
 
 		elif com == 'pimptochannel': #target == "on", otherwise assumed "off"
 			if target == "on":
-				__builtin__.pimpToChandb[chan] = True
+				self.locker.pimpToChandb[chan] = True
 			else:
-				__builtin__.pimpToChandb[chan] = False
+				self.locker.pimpToChandb[chan] = False
 			self.msg(u, "Added to database")
-			pimpToChan(self, "%s <> %s added to __builtin__.pimpToChandb by %s with state op %s" % (ts, chan, u, target))
+			pimpToChan(self, "%s <> %s added to self.locker.pimpToChandb by %s with state op %s" % (ts, chan, u, target))
 
 		else:
 			self.msg(u, "unavailable")
@@ -192,12 +190,12 @@ class api:
 		print "[%s] %s" % (channel, text)
 
 #initialize dbs if they don't exist
-if not hasattr(__builtin__, 'pimpToChandb'):
-	__builtin__.pimpToChandb = {
+if not hasattr(self.locker, 'pimpToChandb'):
+	self.locker.pimpToChandb = {
 		"#secretpimps" : True
 	}
-if not hasattr(__builtin__, 'pimpdb'):
-	__builtin__.pimpdb = {}
+if not hasattr(self.locker, 'pimpdb'):
+	self.locker.pimpdb = {}
 
 if __name__ == "__main__":
 	api = api()
