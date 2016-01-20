7a0b440c46fa2ce4a686b9fa07ffb85cc7f70338
Sean <sean@tox.im>
2016-01-19 19:44:32 -0800

    Add brainfuck module

--- brainfuck.py
+++ brainfuck.py
@@ -3,7 +3,14 @@
 def declare():
     return {"brainfuck": "privmsg"}
 
-def callback(self, type, isop, command="", msg="", user="", channel="", mode=""):
+def callback(self):
+    channel = self.channel
+    command = self.command
+    user = self.user
+    msg = self.message
+    type = self.type
+    isop = self.isop
+
     if channel.startswith('#'):
         DEFAULT_COUNTER = 1000000
         counter = DEFAULT_COUNTER
@@ -14,16 +21,16 @@ def callback(self, type, isop, command="", msg="", user="", channel="", mode="")
         disabled = -1
         i = 0
         c = 0
-        
+
         while i < len(msg):
             c = msg[i]
             i += 1
             counter -= 1
-    
+
             if counter <= 0:
                 self.msg(channel, "Command exceeded %s ticks." % (DEFAULT_COUNTER))
-                return("Command exceeded %s ticks." % (DEFAULT_COUNTER)) 
-            
+                return("Command exceeded %s ticks." % (DEFAULT_COUNTER))
+
             if c == '[':
                 if array[index] == 0:
                     disabled = len(lastRepeat)
@@ -55,15 +62,15 @@ def callback(self, type, isop, command="", msg="", user="", channel="", mode="")
                 output += chr(array[index])
             else:
                 continue
-    
+
             #print(c+":"+str(index)+":"+str(array))
-            
+
             #try:
             if array[index] > 255:
                 array[index] = 0
             elif array[index] < 0:
                 array[index] = 255
-    
+
         self.msg(channel, output[:133])
         return output[:133]
 
@@ -77,17 +84,19 @@ if __name__ == "__main__":
 
     hook = list(declare())[0]
 
-    msg = "^pimp"
-    user = "joe!username@hostmask"
-    channel = "#test"
-    type = "privmsg"
-    isop = True
-    
-    t1 = callback(api, type, isop, command=hook, msg="^brainfuck ++++++++++++++++++++++++++++++++++++++++++++++++++++++++..............", channel=channel, user=user)
-    t2 = callback(api, type, isop, command=hook, msg="^brainfuck ++[++++.]", channel=channel, user=user)
-    t3 = callback(api, type, isop, command=hook, msg="-[-.]", channel=channel, user=user)
-    
-    callback(api, type, isop, command=hook, msg=msg, channel=channel, user=user)
+    setattr(api, 'isop', True)
+    setattr(api, 'type', 'privmsg')
+    setattr(api, 'command', hook)
+    setattr(api, 'user', 'joe!username@hostmask')
+    setattr(api, 'channel', '#test')
+
+    setattr(api, 'message', "^brainfuck ++++++++++++++++++++++++++++++++++++++++++++++++++++++++..............")
+    t1 = callback(api)
+    setattr(api, 'message', "^brainfuck ++[++++.]")
+    t2 = callback(api)
+    setattr(api, 'message', "^brainfuck -[-.]")
+    t3 = callback(api)
+
     if t1 != "88888888888888":
         exit(1)
     if t2 != 'Command exceeded 1000000 ticks.':
@@ -96,4 +105,4 @@ if __name__ == "__main__":
         if t3 != 'þýüûúùø÷öõôóòñðïîíìëêéèçæåäãâáàßÞÝÜÛÚÙØ×ÖÕÔÓÒÑÐÏÎÍÌËÊÉÈÇÆÅÄÃÂÁÀ¿¾½¼»º¹¸·¶µ´³²±°¯®\xad¬«ª©¨§¦¥¤£¢¡\xa0\x9f\x9e\x9d\x9c\x9b':
             exit(4)
     except:
-        api.msg(channel, 'unicode issues in Python cause a passing test to fail')
+        api.msg(api.channel, 'unicode issues in Python cause a passing test to fail')
