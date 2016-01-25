0b318ee9082c98082a2b3aa30070dc8c34d9b793
Sean <sean@tox.im>
2016-01-25 10:17:24 -0800

    Update autovoice.py

--- autovoice.py
+++ autovoice.py
@@ -1,8 +1,9 @@
 channels = {'#fatpeoplesuck' '#fathateelite'}
+ignore = {self.nickname,'}o{','FatStats','justnotfair'}
 
 def declare():
     return {"autovoice": "userjoin"}
 
 def callback(self):
-    if self.channel in channels:
+    if self.channel in channels and not in ignore:
         self.mode(channel ,True, 'v', mask=user)
