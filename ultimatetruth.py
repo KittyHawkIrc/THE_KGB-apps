0fb217a7de0d9be8c4b1b667cda1c552fa00424d
nonillion <jadelyjade1@gmail.com>
2016-01-29 11:49:36 -0500

    Update ultimatetruth.py

--- ultimatetruth.py
+++ ultimatetruth.py
@@ -1,9 +1,9 @@
 def declare():
         return {"ultimatetruth": "privmsg"}
 
-def callback(self, type, isop, command="", msg="", user="", channel="", mode=""):
-        if channel.startswith('#'):
-                return self.msg(channel, "RielDtok is a tranny")
+def callback(self):
+        if self.channel.startswith('#'):
+                return self.msg(self.channel, "RielDtok is a tranny")
 
 class api:
         def msg(self, channel, text):
@@ -13,6 +13,8 @@ if __name__ == "__main__":
         api = api()
         u = "joe!username@hostmask"
         c = '#test'
+	
+	setattr(api,'channel',c)
 
-        if callback(api, '', True, command="ultimatetruth", msg="^ultimatetruth", channel=c, user=u) != "[%s] RielDtok is a tranny" % (c):
+        if callback(api) != "[%s] RielDtok is a tranny" % (c):
                 exit(1)
