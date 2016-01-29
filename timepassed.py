fe15713915589e96128bbafbde943eb72c38131a
Sean <sean@tox.im>
2016-01-29 02:34:07 -0800

    msg fix in time passed

--- timepassed.py
+++ timepassed.py
@@ -13,18 +13,18 @@ def callback(self):
     except:
       self.locker.time[outgoing_channel] = datetime.datetime.now()
       return
-    
+
     diff = datetime.datetime.now() - time
-    
+
     if diff.total_seconds() < 600:
       self.locker.time[outgoing_channel] = datetime.datetime.now()
       return
-    
+
     hours = diff.total_seconds() / 3600
     minutes = hours % 1 * 60
     hours = int(hours)
-    
+
     #set this time in the locker
     self.locker.time[outgoing_channel] = datetime.datetime.now()
-    
-    self.msg(outgoing_channel, 'It's been %s hours and %s minutes since the last message was sent in %s'%(hours, minutes, incoming_channel))
+
+    self.msg(outgoing_channel, "It's been %s hours and %s minutes since the last message was sent in %s" % (hours, minutes, incoming_channel))
