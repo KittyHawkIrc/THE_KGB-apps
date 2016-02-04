1d71a5ff20ca8ff3fc40a1e0ce3c9655ecc0b3d8
Sean <sean@tox.im>
2016-02-04 13:45:00 -0800

    Create fph-time.py

---
+++ fph-time.py
@@ -0,0 +1,9 @@
+import datetime
+
+diff = self.lockerbox['#fatpeoplehate#soopersekrit'].time['#soopersekrit']
+
+hours = diff.total_seconds() / 3600
+minutes = hours % 1 * 60
+hours = int(hours)
+
+print "It's been %s hours and %s minutes since the last message was sent in %s (total %s seconds)" % (hours, minutes, '#FatPeopleHate',diff.total_seconds())
