20dbd419cf3c6eacc311b7a3543e24208b0f94e4
nonillion <jadelyjade1@gmail.com>
2016-02-11 17:34:33 -0500

    Update animalfacts.py

--- animalfacts.py
+++ animalfacts.py
@@ -121,7 +121,7 @@ def declare():
   return {"animalfacts": "privmsg", "sadanimalfacts": "privmsg"}
 
 def callback(self):
-  self.msg(self.channel, "Here's a sad animal fact for you! %s" % (random.choice(factimg))
+  self.msg(self.channel, "Here's a sad animal fact for you! %s" % (random.choice(factimg)))
 
 class api:
   def msg(self, channel, text):
