5b3049c67890f0b3358b100c4ec4add80be7da2f
nonillion <jadelyjade1@gmail.com>
2016-02-19 18:06:14 -0500

    make it only autovoice people who are on the list
    only verified users will now be voiced

--- autovoice.py
+++ autovoice.py
@@ -1,4 +1,4 @@
-ignore = {'invisiblecalories', 'justnotfair', '}o{', 'LimitServ', 'YT-info', 'VALIS', 'FatStats'}
+include = {'cyracs', 'syracs', 'nwo-chan', 'marlow', 'marlzey', 'marlsey', 'rieldtok', 'sirius-cybernetics', 'hydrogen_cyanide', 'thorgin', 'derpydoom', 'doomclass', 'coup', 'coup_de_shitlord', 'bym', 'purpleisafruit', 'creative', 'creativenickname', 'madam_psycho_sexy', 'shitladyoftheskies', 'uvulectomy', 'moudi', 'meeblesmerble', '[deleted]'}
 
 def declare():
     return {"overkill": "userjoin"}
@@ -6,6 +6,6 @@ def declare():
 def callback(self):
 
     if self.channel == '#fatpeoplesuck':
-        u = self.user.split('!',1)[0]
-        if not u in ignore:
+        u = self.user.lower.split('!',1)[0]
+        if u in include:
             self.msg('ChanServ', 'voice %s %s' % (self.channel, u))
