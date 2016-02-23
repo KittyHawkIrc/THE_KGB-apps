dcdcb957709e7a3d4a1edd6ffb47954099c431dd
nonillion <jadelyjade1@gmail.com>
2016-02-22 21:19:08 -0500

    Update seen.py

--- seen.py
+++ seen.py
@@ -15,7 +15,7 @@ def callback(self):
       if u2 == u:
         return self.msg(self.channel, 'Just whois yourself, you fool')
       else:
-        return self.msg(self.channel, '%s was last seen joining %s at %s. Also fuck proper formatting. Who do you think you are, a stripper? You don\'t tell me what to do.' % (u2, users[u2][0], users[u2][1]))
+        return self.msg(self.channel, '%s was last seen joining %s at %s. Also fuck proper formatting. Who do you think you are, a stripper? You don\'t tell me what to do.' % (u2, users[u2][1], users[u2][0]))
     except:
         return self.msg(self.channel, 'I have not seen this person yet. Please try later. Thank you.')
 
