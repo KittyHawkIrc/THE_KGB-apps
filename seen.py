f2a9ad3fc1e1bc463d7272107e55e48947e1c82e
nonillion <jadelyjade1@gmail.com>
2016-02-22 21:08:09 -0500

    initial commit

---
+++ seen.py
@@ -0,0 +1,43 @@
+import datetime
+users = {}
+
+def declare():
+    return {"seen": "userjoin", "seen": "privmsg"}
+
+def callback(self):
+  u = self.user.split('!')[0]
+  
+  if self.type == 'userjoin':
+    users[u] = [datetime.datetime.now(),self.channel]
+  else:
+    u2 = self.message.split(' ')[1]
+    try:
+      if u2 == u:
+        self.msg('Just whois yourself, you fool')
+      else:
+        self.msg('%s was last seen joining %s at %s. Also fuck proper formatting. Who do you think you are, a stripper? You don't tell me what to do.' % (u2, users[u2][0], users[u2][1]))
+    except:
+        return self.msg('I have not seen this person yet. Please try later. Thank you.')
+
+class api:
+    def msg(self, channel, text):
+        return "[%s] %s" % (channel, text)
+
+if __name__ == "__main__":
+    api = api()
+    setattr(api, 'type', 'userjoin')
+    setattr(api, 'user', 'joe!username@hostmask')
+    setattr(api, 'channel', '#test')
+    setattr(api, 'ver', '1.1.8')
+    callback(api)
+    setattr(api, 'type', 'privmsg')
+    setattr(api, 'message', '^seen joe')
+    
+    if 'Just whois yourself, you fool' not in callback(api):
+        exit(1)
+    setattr(api, 'user', 'john!username@hostmask')
+    if 'joe was last seen joining #test at' not in callback(api):
+        exit(2)
+    setattr(api, 'message', '^seen jack')
+    if 'I have not seen this erson yet. Please try later. Thank you' not in callback(api):
+        exit(3)
