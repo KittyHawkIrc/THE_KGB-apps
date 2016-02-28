f804da47279982844221471f4b79dd4062c7bfc9
nonillion <jadelyjade1@gmail.com>
2016-02-28 09:36:49 -0500

    initial commit of BMI storage feature

--- bmi.py
+++ bmi.py
@@ -23,27 +23,43 @@ def declare():
     return {'bmi': 'privmsg'}
 
 def callback(self):
-    mass = 0.0
-    height = 0.0
-    bmi = 0.0
-
-    message = self.message.split(self.command, 1)[1]
-
-    parameters = parseMessage(message)
-
-    for parameter in parameters:
-        if parameter[1] in heightUnits:
-            for unit in heights:
-                if parameter[1] in unit[:-1]:
-                    height += parameter[0] * unit[-1]
-        elif parameter[1] in massUnits:
-            for unit in masses:
-                if parameter[1] in unit[:-1]:
-                    mass += parameter[0] * unit[-1]
-        elif parameter[1].lower() == 'bmi':
-            bmi = parameter[0]
-
-    if height > 0 and mass >= 0 and bmi == 0:
+    u = self.message.split('!').lower()[0]
+    p1 = self.message.split(' ').lower()[1]
+    
+    if p1 in self.locker.bmi:
+        try:
+			bmi = self.locker.bmi[u]
+		except:
+			try:
+				_ = self.locker.bmi
+			except:
+				self.locker.bmi = dict()
+			return self.msg(channel, "%s has yet to set their BMI" % (p1.capitalize))
+		
+        if bmi < 18.5:
+            o = '\002\00308underweight'
+        elif bmi > 24:
+            o = '\002\00307close to overweight'
+        elif bmi > 25:
+            o = '\002\00304overweight'
+        else:
+            o = '\002\00309in a normal healthy range'
+        
+        return self.msg(self.channel, '%s\'s BMI is %s. This BMI is %s.' % (p1.capitalize(), format(bmi,'\.2f'), o)
+    
+    c = calc(self)
+    bmi = c[0]
+    mass = c[1]
+    height = c[2]
+    
+    if p1 == 'set':
+        try:
+            self.locker.bmi[u] = mass / (height ** 2)
+        except:
+            self.locker.bmi = {u : mass / (height ** 2)}
+        
+        return self.msg(self.channel,"Your BMI is set to be %s" % (format(self.locker.bmi[u],'\.2f')))
+    elif height > 0 and mass >= 0 and bmi == 0:
         bmi = mass / (height ** 2)
 
         output = 'Your BMI is %s, you are \002\003' % format(bmi, '.2f')
@@ -67,6 +83,28 @@ def callback(self):
 
         return self.msg(self.channel, 'Your height is %sm' % format(height, '.2f'))
 
+def calc(message):
+    mass = 0.0
+    height = 0.0
+    bmi = 0.0
+    
+    message = self.message.split(self.command, 1)[1]
+    
+    parameters = parseMessage(message)
+
+    for parameter in parameters:
+        if parameter[1] in heightUnits:
+            for unit in heights:
+                if parameter[1] in unit[:-1]:
+                    height += parameter[0] * unit[-1]
+        elif parameter[1] in massUnits:
+            for unit in masses:
+                if parameter[1] in unit[:-1]:
+                    mass += parameter[0] * unit[-1]
+        elif parameter[1].lower() == 'bmi':
+            bmi = parameter[0]
+    
+    return [bmi,mass,height]
 
 def split(text, separators):
     for separator in separators:
@@ -104,13 +142,19 @@ class api:
     return "[%s] %s" % (channel, text)
 
 if __name__ == "__main__":
-  api = api()
-  setattr(api, 'isop', True)
-  setattr(api, 'type', 'privmsg')
-  setattr(api, 'command', 'bmi')
-  setattr(api, 'user', 'joe!username@hostmask')
-  setattr(api, 'channel', "#test")
-  setattr(api, 'message', '^bmi 5\'6\" 130lbs')
+    api = api()
+    setattr(api, 'isop', True)
+    setattr(api, 'type', 'privmsg')
+    setattr(api, 'command', 'bmi')
+    setattr(api, 'user', 'joe!username@hostmask')
+    setattr(api, 'channel', "#test")
+    setattr(api, 'message', '^bmi 5\'6\" 130lbs')
 
-  if "Your BMI is" not in callback(api):
-    exit(1)
+    if "Your BMI is 20.98" not in callback(api):
+        exit(1)
+    setattr(api, 'message', '^bmi set 5\'6\" 130lbs')
+    if "Your BMI is set to be 20.98" not in callback(api):
+        exit(2)
+    setattr(api, 'message', '^bmi joe')
+    if "Joe's BMI is 20.98" not in callback(api):
+        exit(3)
