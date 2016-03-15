a079a7b52ecd494d59bd567d096c93176165a38a
Brian W <bwongggg@gmail.com>
2016-03-15 17:53:37 -0400

    changed from python 3.4 to 2.7

--- bmi.py
+++ bmi.py
@@ -142,7 +142,7 @@ def parseMessage(message):
 
 class api:
 	def msg(self, channel, text):
-		print ("[%s] %s" % (channel, text))
+		print "[%s] %s" % (channel, text)
 		return "[%s] %s" % (channel, text)
 class empty:
 	pass
