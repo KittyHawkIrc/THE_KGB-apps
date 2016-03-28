9c7478fe8150dfc1ceedf57c886db8b4641b519d
Brian W <bwongggg@gmail.com>
2016-03-28 19:41:15 -0400

    close urlopen

--- ud.py
+++ ud.py
@@ -6,23 +6,21 @@ def declare():
   return {"ud": "privmsg"}
 
 def callback(self):
-    try:
-        r = urllib2.urlopen('http://api.urbandictionary.com/v0/define?term=%s' % '+'.join(self.message.split(' ')[1:]))
-        data = json.load(r)
-        if data['result_type'] != 'no_results':
-            defLines = data['list'][0]['definition'].splitlines()
-            for line in defLines:
-                if line[-1] not in ',.?!':
-                    line = line + ','
-            definition = '%s: %s %s' % (self.message, data['list'][0]['permalink'], ' '.join(defLines))
+    r = urllib2.urlopen('http://api.urbandictionary.com/v0/define?term=%s' % '+'.join(self.message.split(' ')[1:]))
+    data = json.load(r)
+    r.close()
+    if data['result_type'] != 'no_results':
+        defLines = data['list'][0]['definition'].splitlines()
+        for line in defLines:
+            if line[-1] not in ',.?!':
+                line = line + ','
+        definition = '%s: %s %s' % (self.message, data['list'][0]['permalink'], ' '.join(defLines))
 
-            if len(definition) > maxChars:
-                definition = definition[:maxChars-4] + '...'
-            return self.msg(self.channel, definition)
-        else:
-            return self.msg(self.channel, 'No definition for %s.' % self.message)
-    except:
-		return self.msg(self.channel, 'I cannot fetch this definition at the moment.')
+        if len(definition) > maxChars:
+            definition = definition[:maxChars-4] + '...'
+        return self.msg(self.channel, definition)
+    else:
+        return self.msg(self.channel, 'No definition for %s.' % self.message)
 
 class api:
 	def msg(self, channel, text):
