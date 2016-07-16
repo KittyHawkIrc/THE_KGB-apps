459715c56c6cfb0d47d134b68cddcd4663385617
Sean <sean@tox.im>
2016-07-15 23:52:01 -0600

    Fix tests

--- bmi.py
+++ bmi.py
@@ -236,7 +236,11 @@ while(True):
 		print callback(api)
 '''
 if __name__ == "__main__":
+	def cache_save():
+		print 'Cache saved'
+		
 	api = api()
+	setattr(api, 'cache_save', cache_save)
 	setattr(api, 'type', 'privmsg')
 	setattr(api, 'channel', "#test")
 	setattr(api, 'locker', empty)
