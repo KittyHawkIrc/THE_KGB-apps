bd2145ae73a52a98a70cf575497c5f185b723348
Brian W <bwongggg@gmail.com>
2016-07-18 16:21:02 -0400

    commented out interactive testing

--- weather.py
+++ weather.py
@@ -104,7 +104,7 @@ class api:
 class empty:
 	pass
 
-
+'''
 # interactive testing:
 api = api()
 def cache_save():
@@ -123,6 +123,7 @@ while(True):
         setattr(api, 'command', 'w')
     setattr(api, 'message', _input)
     print callback(api)
+'''
 
 if __name__ == "__main__":
     def cache_save():
