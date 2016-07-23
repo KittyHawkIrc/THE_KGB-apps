0768adaefc9a2086a8cb785840df72c8d40e72c7
Brian W <bwongggg@gmail.com>
2016-07-23 17:37:30 -0400

    more closely match behaviour of real commands

--- speshulcmds.py
+++ speshulcmds.py
@@ -6,14 +6,26 @@ def declare():
     return {"ultimatetruth": "privmsg", 'msc': 'privmsg'}
 
 def callback(self):
+    message = self.message.split(self.command, 1)[1].strip()
+
     if self.command.lower() == 'ultimatetruth':
-        return self.msg(self.channel, "RielDtok is a tranny")
+        return self.msg(self.channel, emulate(message, 'RielDtok is a tranny'))
     if self.command.lower() == 'msc':
-        return self.msg(self.channel, '^billdred\n<MsC> I like it black like my dicks? <billdred> we agree on that too!')
+        return self.msg(self.channel, emulate(message, '^billdred\n<MsC> I like it black like my dicks? <billdred> we agree on that too!'))
+
+def emulate(message, output):
+    try:
+        try:
+            return '%s: %s' % (message.split()[0], output.split('\n')[0])
+        except:
+            return '%s: %s' % (message.split()[0], output)
+    except:
+        return output
+
 
 class api:
-    def msg(self, channel, text):
-        return "[%s] %s" % (channel, text)
+	def msg(self, channel, text):
+		return "[%s] %s" % (channel, text)
 
 if __name__ == "__main__":
     api = api()
@@ -22,14 +34,27 @@ if __name__ == "__main__":
 
     setattr(api, 'isop', True)
     setattr(api, 'type', 'privmsg')
-    setattr(api, 'command', 'ultimatetruth')
     setattr(api, 'user', 'joe!username@hostmask')
     setattr(api, 'channel', '#test')
+
+    setattr(api, 'command', 'ultimatetruth')
+    setattr(api, 'message', '^ultimatetruth')
     print callback(api)
     if callback(api) != "[%s] RielDtok is a tranny" % (c):
         exit(1)
 
+    setattr(api, 'message', '^ultimatetruth cats')
+    print callback(api)
+    if 'cats: RielDtok' not in callback(api):
+        exit(2)
+
     setattr(api, 'command', 'MsC')
+    setattr(api, 'message', '^MsC')
     print callback(api)
     if '^billdred\n<MsC>' not in callback(api):
-        exit(2)
+        exit(3)
+
+    setattr(api, 'message', '^MsC cats')
+    print callback(api)
+    if 'cats: ^billdred' != callback(api):
+        exit(4)
