37314771d93e9c7f9f98aa65ce2b48ec23733e15
Sean <sean@tox.im>
2016-07-27 04:11:56 -0700

    Prettier temperature formatting, no (s)

--- convert.py
+++ convert.py
@@ -12,7 +12,7 @@ unit = UnitRegistry() #can't code for shit
 convert = unit.Quantity #like how do you fuck up conversions that bad
 
 def declare():  #SERIOUSLY. no one used the 30000 hooks in here.
-    return {"convert":"privmsg", "fuckbym":"privmsg", "c": "privmsg"}
+    return {"convert":"privmsg", "fuckbym":"privmsg", "c":"privmsg"}
 
 def tempconv(unit):     #the library is a little iffy on names
     c = ['c', 'celc', 'celsius']
@@ -37,22 +37,25 @@ def tempconv(unit):     #the library is a little iffy on names
     else:
         return unit
 
-def prettytemp(unit):
+def prettytemp(unit, num):
 
     if unit == 'degC':
-        return 'celsius'
+        return 'C'
 
     elif unit == 'degF':
-        return 'fahrenheit'
+        return 'F'
 
     elif unit == 'degK':
-        return 'kelvin'
+        return 'K'
 
     elif unit == 'degR':
-        return 'rankine'
+        return 'R'
 
     else:
-        return unit
+        if num == 1:
+            return unit
+        else:
+            return unit + 's'
 
 def stringparse(text): #it took me 15 mintues to write a better string parser than bym took a week
     text_list = text.split()
@@ -84,20 +87,24 @@ def stringparse(text): #it took me 15 mintues to write a better string parser th
 
 
 def callback(self): #remove_bym
-    unit1, num, unit2 = stringparse(self.message.split(' ', 1)[1])
+    try:
+        unit1, num, unit2 = stringparse(self.message.split(' ', 1)[1])
+    except:
+        return self.msg(self.msg, 'You have to actually add things after the command')
 
     if unit1 == False:
         return self.msg(self.channel, "Error, it doesn't look like you're trying to convert properly")
 
     try:
         num, unit2 = '{0}'.format(convert(num, unit(unit1)).to(unit2)).split() #dirty, but it works perfectly
-        unit2 = prettytemp(unit2)                                               #things bym can't say ^
+
         num = round(float(num), 3)
+        unit2 = prettytemp(unit2, num)                                               #things bym can't say ^
 
     except:
         return self.msg(self.channel, 'Conversion error!')
 
-    return self.msg(self.channel, "%s %s(s)" % (num, unit2))
+    return self.msg(self.channel, "%s %s" % (num, unit2))
 
 
 class api: #You're now known as remove_bym
@@ -114,17 +121,17 @@ if __name__ == "__main__":
     setattr(api, 'user', 'joe!username@hostmask')
     setattr(api, 'message', '^convert 18cm in')
 
-    if not 'inch' in callback(api):
+    if not 'inchs' in callback(api):
         exit(1)
 
     setattr(api, 'message', '^convert 18 cm to in')
-    if not 'inch' in callback(api):
+    if not 'inchs' in callback(api):
         exit(2)
 
     setattr(api, 'message', '^convert 18 cm in')
-    if not 'inch' in callback(api):
+    if not 'inchs' in callback(api):
         exit(3)
 
     setattr(api, 'message', '^convert 18cm in in')
-    if not 'inch' in callback(api):
+    if not 'inchs' in callback(api):
         exit(4)
