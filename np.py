a52d1a4d4186018fc4379dd8b1cff4c900189cd4
Hannah W <bwongggg@gmail.com>
2017-08-06 01:07:40 -0400

    Update np.py

--- np.py
+++ np.py
@@ -109,7 +109,7 @@ if __name__ == '__main__':
     setattr(api, 'isop', False)
     setattr(api, 'isowner', False)
 ###############nick########### START: Interactive Testing ##########################
-'''
+    '''
     while(True):
         _input = raw_input('Enter message here: ')
         input_split = _input.split()
@@ -141,7 +141,7 @@ if __name__ == '__main__':
             setattr(api, 'message', _input)
             print callback(api)
             continue
-'''
+    '''
 ########################### END: Interactive Testing ###########################
     setattr(api, 'command', 'np')
     setattr(api, 'message', '^np')
