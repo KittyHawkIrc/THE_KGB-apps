315b58f6d3eb1b5924de291ab0fd798ba292b2ac
Sean <sean@tox.im>
2018-01-24 10:20:25 -0800

    Update autovoice for profiles

--- autovoice.py
+++ autovoice.py
@@ -1,32 +1,11 @@
-import urllib2
-
 #Update schema
 __url__ = "https://raw.githubusercontent.com/KittyHawkIrc/modules/production/" + __name__ + ".py"
-__version__ = 1.0
-
-req = urllib2.Request('https://raw.githubusercontent.com/KittyHawkIrc/FPS-verified/master/users.txt')
-
+__version__ = 2.0
 
 
 def declare():
     return {"overkill": "userjoin"}
 
-def nounderscore(u):
-    if u[len(u) - 1] == '_':
-        return nounderscore(u[:len(u) - 1])
-    else:
-        return u
-
 def callback(self):
-    if self.channel == '#fatpeoplesuck':
-
-        fd = urllib2.urlopen(req)
-        isverified = eval(fd.read())
-        fd.close()
-
-        u = self.user.split('!',1)[0].lower()
-
-        if nounderscore(u) in isverified:
-            self.msg('ChanServ', 'voice %s %s' % (self.channel, u))
-        elif u+'_' in isverified:
-            self.msg('Chanserv', 'voice %s %s' % (self.channel, u+'_'))
+    if self.channel == '#fatpeoplesuck' and self.profile.isverified:
+        self.mode(self.channel, True, 'v', user=self.profile.nickname)
