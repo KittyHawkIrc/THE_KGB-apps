6b941c9926df0787eb497bfb9229c0ea91b265d5
Sean <sean@tox.im>
2018-01-24 11:10:55 -0800

    Verified command

--- autovoice.py
+++ autovoice.py
@@ -1,32 +1,52 @@
-import urllib2
+# -*- coding: utf-8 -*-
 
-#Update schema
 __url__ = "https://raw.githubusercontent.com/KittyHawkIrc/modules/production/" + __name__ + ".py"
-__version__ = 1.0
-
-req = urllib2.Request('https://raw.githubusercontent.com/KittyHawkIrc/FPS-verified/master/users.txt')
-
+__version__ = 2.0
 
 
 def declare():
-    return {"overkill": "userjoin"}
-
-def nounderscore(u):
-    if u[len(u) - 1] == '_':
-        return nounderscore(u[:len(u) - 1])
-    else:
-        return u
+    return {"autovoice": "userjoin", "verify": "privmsg"}
 
 def callback(self):
-    if self.channel == '#fatpeoplesuck':
+    if self.type == 'userjoin':
+        if self.channel == '#fatpeoplesuck' and self.profile.isverified:
+            self.mode(self.channel, True, 'v', user=self.profile.nickname)
+    else:
+        privlist = self.message.split()
 
-        fd = urllib2.urlopen(req)
-        isverified = eval(fd.read())
-        fd.close()
+        if len(privlist) == 1: #^verify
+            return self.msg(self.channel, '%s: You %s verified.' % (
+            self.profile.nickname, ("are" if self.profile.isverified else "are not")))
 
-        u = self.user.split('!',1)[0].lower()
+        elif len(privlist) == 2:
+            selector = self.profileManager.getuser_bynick(privlist[1])
 
-        if nounderscore(u) in isverified:
-            self.msg('ChanServ', 'voice %s %s' % (self.channel, u))
-        elif u+'_' in isverified:
-            self.msg('Chanserv', 'voice %s %s' % (self.channel, u+'_'))
+            if selector:
+                return self.msg(self.channel, '%s: %s %s verified.' % (
+                self.profile.nickname, selector.nickname, ("is" if selector.isverified else "is not")))
+
+            else:
+                return self.msg(self.channel, "%s: That user couldn't be found." % (
+                self.profile.nickname))
+
+        elif len(privlist) == 3:
+
+            if self.isop:
+                selector = self.profileManager.getuser_bynick(privlist[1])
+
+                if selector:
+                    if 'y' in privlist[2].lower():
+                        self.profileManager.update(selector.username, isverified=True)
+                    elif 'n' in privlist[2].lower():
+                        self.profileManager.update(selector.username, isverified=False)
+
+                    return self.msg(self.channel, "%s: %s %s verified." % (
+                        self.profile.nickname, selector.nickname, ("is now" if self.profileManager.getuser_bynick(privlist[1]).isverified else "is no longer")))
+
+                else:
+                    return self.msg(self.channel, "%s: That user couldn't be found." % (
+                        self.profile.nickname))
+
+            else:
+                return self.msg(self.channel, "%s: You don't have permissions to verify others" % (
+                    self.profile.nickname))
